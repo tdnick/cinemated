ec8300a10202023fdf9661263b1e9ae1fd797366
tdnick <iliescu.nicolas@gmail.com>
2020-05-05 15:07:45 +0300

    Baza initiala 5 mai

---
+++ public/scripts/filme.js
@@ -0,0 +1,53 @@
+window.onload = function(){
+	var filter = document.getElementById("filter_icon");
+	var apasat = false;
+	filter.onclick = function(){
+		var continut_filtrare = document.getElementById("continut_filtrare");
+		if(apasat == false){
+			continut_filtrare.style.display = "block";
+			apasat = true;
+		}
+		else {
+			continut_filtrare.style.display = "none";
+			apasat = false;
+		}
+	}
+	
+	var checkboxes = document.querySelectorAll("input[type = 'checkbox']");
+	
+	var buton_filtrare = document.getElementById("buton_filtrare");
+	buton_filtrare.onclick = function() {
+		
+		var neselectat = false;
+		for(let i = 0; i < checkboxes.length; i++)
+			if(checkboxes[i].checked == true){
+				neselectat = true;
+				break;
+			}
+		if(neselectat == true){
+			for(i = 0; i < checkboxes.length; i++){
+				var valoare = checkboxes[i].value;
+				var sectiune = document.getElementById(valoare);
+				if(checkboxes[i].checked == false)
+					sectiune.style.display = "none";
+				else 
+					sectiune.style.display = "block";
+					
+			}	
+		}
+		
+		
+	}
+	
+	var buton_resetare = document.getElementById("buton_resetare");
+	buton_resetare.onclick = function(){
+		for(let i = 0; i < checkboxes.length; i++) {
+			var valoare = checkboxes[i].value;
+			var sectiune = document.getElementById(valoare);
+			sectiune.style.display = "block";
+			checkboxes[i].checked = false;
+		}
+	}
+	
+	
+}
