d71dfe12492a75fc96d88efc872d85591101d2c9
tdnick <iliescu.nicolas@gmail.com>
2020-05-21 23:08:24 +0300

    Update interfata contul meu, modificare date utilizator, modificare parola BETA
    la modificare parola sunt niste buguri destul de serioase la validari etc, dar daca puneti datele corecte, merge :))

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const util = require('util');
 const crypto = require('crypto');
 const url = require('url');
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(session({
+    secret: 'abcdefg', // for login
+    resave: true,
+    saveUninitialized: false
+}));
+
+// database configuration
+
 oracledb.autoCommit = true;
 
 var connectionProperties = {
@@ -27,110 +38,58 @@ function doRelease(connection) {
     });
 };
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+// GET requests
 
-app.use(session({
-    secret: 'abcdefg', // for login
-    resave: true,
-    saveUninitialized: false
-}));
 app.get("/choose", function(req, res) {
     res.render("html/choose", {});
 });
+
 app.get("/confirm", function(req, res) {
     res.render("html/confirm", {});
 });
+
 app.get("/locuri", function(req, res) {
     res.render("html/locuri", {});
 });
+
 app.get("/last", function(req, res) {
     res.render("html/last", {});
 });
+
 app.get("/", function (req, res) {
-    res.render("html/index", {user: req.session.username});
+    res.render("html/index", {user: req.session.userData});
 });
 
 app.get("/login", function (req, res) {
     res.render("html/login", {});
 });
 
-app.get("/dashboard", function (req, res) {
-    oracledb.getConnection(connectionProperties, function (err, connection) {
-        if (err) {
-            console.error(err.message);
-            res.status(500).send("Error connecting to DB");
-            return;
-        }
-        if (req.session.username){
-            var dbRequest = "SELECT * FROM users WHERE username = '" + req.session.username + "'";
-            connection.execute(dbRequest, {},
-                { outFormat: oracledb.OBJECT },
-                function (err, result) {
-                    if (err) {
-                        console.error(err);
-                        res.status(500).send("Eroare");
-                        doRelease(connection);
-                        return;
-                    }
-                    result.rows.forEach(function (element) {
-                        ret = {name: element.FULL_NAME, email: element.EMAIL};
-                    }, this);
-                    doRelease(connection);
-                    console.log("Got user data");
-                    console.log(ret);
-                    res.render("html/dashboard", {user: req.session.username, userData: ret});
-            });
-        }
-        else {
-            doRelease(connection);
-            res.render("html/dashboard", {user: req.session.username});
-        }
-    });
+app.get("/dashboard/info", function (req, res) {
+    res.render("html/dashboard/info", {user: req.session.userData});
+})
+
+app.get('/dashboard', function (req, res) {
+    res.redirect("/dashboard/info");
+})
+
+app.get('/dashboard/security', function (req, res) {
+    res.render("html/dashboard/security", {user: req.session.userData});
+})
+
+app.get('/dashboard/limitless', function (req, res) {
+    res.render("html/dashboard/limitless", {user: req.session.userData});
+})
+
+app.get('/dashboard/tickets', function (req, res) {
+    res.render("html/dashboard/tickets", {user: req.session.userData});
+})
+
+app.get('/dashboard/settings', function (req, res) {
+    res.render("html/dashboard/settings", {user: req.session.userData});
 })
 
 app.get("/register", function (req, res) {
     res.render("html/register", {});
-});
-
-app.post("/register", function (req, res) {
-    var form = new formidable.IncomingForm();
-    var control = 0; // not a problem
-    form.parse(req, function (err, fields, files) {
-        oracledb.getConnection(connectionProperties, function (err, connection) {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Error connecting to DB");
-                return;
-            }
-            var cipher = crypto.createCipher('aes-128-cbc', 'mypassword');
-            var encrPass= cipher.update(fields.pass, 'utf8', 'hex');
-            encrPass += cipher.final('hex');
-            var dbRequest = "insert into users (username, email, password, full_name) values ('" + fields.username + "', '" + fields.email + "', '" + encrPass + "', '" + fields.fname + "')";
-            connection.execute(dbRequest, {},
-                { outFormat: oracledb.OBJECT },
-                function (err, result) {
-                    if (err) {
-                        console.error(err);
-                        if (err.message.localeCompare("ORA-00001: unique constraint (VERONICACHIRUT.USERS_UK2) violated") == 0){
-                            control = 1; // username already used
-                        } else if (err.message.localeCompare("ORA-00001: unique constraint (VERONICACHIRUT.USERS_UK1) violated") == 0){
-                            control = 2; // email address already used
-                        } else {
-                            control = -1;
-                        }
-                        res.render('html/register', {cntl: control});
-                        doRelease(connection);
-                        return;
-                    }
-                    doRelease(connection);
-                    console.log("User registered successfully");
-                    if (control == 0){
-                        res.redirect('index');
-                    }
-            });
-        });
-    });
 });
 
 app.get('/logout', function(req, res) {
@@ -139,60 +98,8 @@ app.get('/logout', function(req, res) {
     res.render('html/logout');
 });
 
-app.post('/login', function (req, res) {
-    var form = new formidable.IncomingForm();
-    var control = 0; // default code for general authentication error
-    form.parse(req, function(err, fields, files) {
-        oracledb.getConnection(connectionProperties, function (err, connection) {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Error connecting to DB");
-                return;
-            }
-            var dbRequest = "SELECT * FROM users WHERE username = '" + fields.username + "'";
-            connection.execute(dbRequest, {},
-                { outFormat: oracledb.OBJECT },
-                function (err, result) {
-                    if (err) {
-                        console.error(err.message);
-                        res.status(500).send("Error getting data from DB");
-                        doRelease(connection);
-                        return;
-                    }
-                    var cipher = crypto.createCipher('aes-128-cbc', 'mypassword');
-                    var encrPass = cipher.update(fields.pass, 'utf8', 'hex');
-                    encrPass += cipher.final('hex');
-                    if (result.rows.length == 0){
-                        control = 3; // username was not found in the database
-                    }
-                    result.rows.forEach(function (element) {
-                        if (element.PASSWORD == encrPass){
-                            //aici trebuie neaparat cu litere mari pt ca altfel nu face verificarea cum trb, imi pare rau de coding style :))
-                            // e okay, nu sunt un coding style nazi :))))
-                            // ms pwp apreciez
-							// cu placere ;)
-                            control = 1; // valid login data
-                        }
-                        else {
-                            console.log("User tried to log in with wrong password");
-                            control = 2; // the user exists but the password is wrong
-                        }
-                    }, this);
-                    doRelease(connection);
-                    if (control == 1){
-                        req.session.username = fields.username;
-                        console.log("User logged in");
-                        res.redirect('/index');
-                    } else {
-                        res.render("html/login", {cntl: control});
-                    }
-            });
-        });
-    });
-});
-
 app.get("/index", function (req, res) {
-    res.render("html/index", {user: req.session.username});
+    res.render("html/index", {user: req.session.userData});
 });
 
 app.get("/filme", function (req, res) {
@@ -242,7 +149,7 @@ app.get("/filme", function (req, res) {
                 }, this);
                 doRelease(connection);
                 console.log("Got movies data");
-                res.render("html/filme", { user: req.session.username, moviesData: movies });
+                res.render("html/filme", { user: req.session.userData, moviesData: movies });
             });
     });
 });
@@ -286,22 +193,221 @@ app.get("/film", function (req, res) {
                     doRelease(connection);
                     console.log("Got movie data");
                     console.log(ret);
-                    res.render("html/film", { user: req.session.username, movieData: ret });
+                    res.render("html/film", { user: req.session.userData, movieData: ret });
                 });
         } else {
             var ret = { name: "", genre: "" }
             doRelease(connection);
-            res.render("html/film", { user: req.session.username, movieData: ret });
+            res.render("html/film", { user: req.session.userData, movieData: ret });
         }
     });
 });
 
 app.get("/despre", function(req, res) {
-    res.render("html/despre", {user: req.session.username});
+    res.render("html/despre", {user: req.session.userData});
 });
 
 app.get("/limitless", function(req, res) {
-    res.render("html/limitless", {user: req.session.username});
+    res.render("html/limitless", {user: req.session.userData});
+});
+
+// POST requests
+
+app.post("/register", function (req, res) {
+    var form = new formidable.IncomingForm();
+    var control = 0; // not a problem
+    form.parse(req, function (err, fields, files) {
+        oracledb.getConnection(connectionProperties, function (err, connection) {
+            if (err) {
+                console.error(err.message);
+                res.status(500).send("Error connecting to DB");
+                return;
+            }
+            var cipher = crypto.createCipher('aes-128-cbc', 'mypassword');
+            var encrPass= cipher.update(fields.pass, 'utf8', 'hex');
+            encrPass += cipher.final('hex');
+            var dbRequest = "insert into users (username, email, password, full_name) values ('" + fields.username + "', '" + fields.email + "', '" + encrPass + "', '" + fields.fname + "')";
+            connection.execute(dbRequest, {},
+                { outFormat: oracledb.OBJECT },
+                function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        if (err.message.localeCompare("ORA-00001: unique constraint (VERONICACHIRUT.USERS_UK2) violated") == 0){
+                            control = 1; // username already used
+                        } else if (err.message.localeCompare("ORA-00001: unique constraint (VERONICACHIRUT.USERS_UK1) violated") == 0){
+                            control = 2; // email address already used
+                        } else {
+                            control = -1;
+                        }
+                        res.render('html/register', {cntl: control});
+                        doRelease(connection);
+                        return;
+                    }
+                    doRelease(connection);
+                    console.log("User registered successfully");
+                    if (control == 0){
+                        res.redirect('index');
+                    }
+            });
+        });
+    });
+});
+
+app.post("/dashboard/security", function (req, res) {
+    //are buguri destul de mari dar merge
+    var form = new formidable.IncomingForm();
+    var control = 0;
+    form.parse(req, function (err, fields, files) {
+        
+        if (fields.newpass != fields.repeat) {
+            control = 1; // passwords do not match
+            console.log("nu sunt la fel");
+            res.render('html/dashboard/security', {cntl: control, user: req.session.userData});
+            return;
+        }
+        
+        oracledb.getConnection(connectionProperties, function (err, connection) {
+            if (err) {
+                console.error(err.message);
+                res.status(500).send("Error connecting to DB");
+                return;
+            }
+            
+            var cipher1 = crypto.createCipher('aes-128-cbc', 'mypassword');
+            var oldPassword = cipher1.update(fields.oldpass, 'utf8', 'hex');
+            oldPassword += cipher1.final('hex');
+            
+            var cipher2 = crypto.createCipher('aes-128-cbc', 'mypassword');
+            var newPassword = cipher2.update(fields.newpass, 'utf8', 'hex');
+            newPassword += cipher2.final('hex');
+            
+            var passRequest = "select * from users where username = '" + req.session.userData.username + "'";
+            
+            connection.execute(passRequest, {}, {outFormat: oracledb.OBJECT}, function (err, result) {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send("Error getting current password");
+                    doRelease(connection);
+                    return;
+                }
+                result.rows.forEach(function (element) {
+                    ret = element.PASSWORD;
+                }, this);
+                //doRelease(connection);
+                console.log("Got password from user");
+                if (ret != oldPassword){
+                    console.log("wrong old pass");
+                    control = 2; //wrong old password
+                    doRelease(connection);
+                }
+            });
+            
+            if (control == 0){
+                var dbRequest = "update users set password = '" + newPassword + "' where username = '" + req.session.userData.username + "'";
+                console.log("username activ: " + req.session.userData.username);
+                connection.execute(dbRequest, {}, {outFormat: oracledb.OBJECT}, function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send("Error updating db");
+                        doRelease(connection);
+                        return;
+                    }
+                    doRelease(connection);
+                    console.log("Password updated successfully");
+                    res.render('html/dashboard/security', {cntl: control, user: req.session.userData});
+                });
+            } else {
+                res.render('html/dashboard/security', {cntl: control, user: req.session.userData});
+            }
+        });
+    });
+});
+
+app.post('/dashboard/info', function (req, res) {
+    var form = new formidable.IncomingForm();
+    var control = 0;
+    form.parse(req, function (err, fields, files) {
+        oracledb.getConnection(connectionProperties, function (err, connection) {
+            if (err) {
+                console.error(err.message);
+                res.status(500).send("Error connecting to DB");
+                return;
+            }
+            var dbRequest = "update users set full_name = '" + fields.fname + "', email = '" + fields.email + "' where username = '" + req.session.userData.username + "'";
+            console.log("username activ: " + req.session.userData.username);
+            connection.execute(dbRequest, {}, {outFormat: oracledb.OBJECT}, function (err, result) {
+                if (err) {
+                    console.error(err);
+                    if (err.message.localeCompare("ORA-00001: unique constraint (VERONICACHIRUT.USERS_UK1) violated") == 0){
+                        control = 1; //email address already used
+                    }
+                    res.render('html/dashboard/info', {cntl: control, user: req.session.userData});
+                    doRelease(connection);
+                    return;
+                }
+                doRelease(connection);
+                req.session.userData.name = fields.fname;
+                req.session.userData.email = fields.email;
+                console.log("User data updated successfully");
+                if (control == 0){
+                    res.render('html/dashboard/info', {cntl: control, user: req.session.userData});
+                }
+            })
+        })
+    })
+})
+
+app.post('/login', function (req, res) {
+    var form = new formidable.IncomingForm();
+    var control = 0; // default code for general authentication error
+    form.parse(req, function(err, fields, files) {
+        oracledb.getConnection(connectionProperties, function (err, connection) {
+            if (err) {
+                console.error(err.message);
+                res.status(500).send("Error connecting to DB");
+                return;
+            }
+            var dbRequest = "SELECT * FROM users WHERE username = '" + fields.username + "'";
+            connection.execute(dbRequest, {},
+                { outFormat: oracledb.OBJECT },
+                function (err, result) {
+                    if (err) {
+                        console.error(err.message);
+                        res.status(500).send("Error getting data from DB");
+                        doRelease(connection);
+                        return;
+                    }
+                    var cipher = crypto.createCipher('aes-128-cbc', 'mypassword');
+                    var encrPass = cipher.update(fields.pass, 'utf8', 'hex');
+                    encrPass += cipher.final('hex');
+                    if (result.rows.length == 0){
+                        control = 3; // username was not found in the database
+                    }
+                    result.rows.forEach(function (element) {
+                        ret = {username: element.USERNAME, name: element.FULL_NAME, email: element.EMAIL};
+                        if (element.PASSWORD == encrPass){
+                            //aici trebuie neaparat cu litere mari pt ca altfel nu face verificarea cum trb, imi pare rau de coding style :))
+                            // e okay, nu sunt un coding style nazi :))))
+                            // ms pwp apreciez
+							// cu placere ;)
+                            control = 1; // valid login data
+                        }
+                        else {
+                            console.log("User tried to log in with wrong password");
+                            control = 2; // the user exists but the password is wrong
+                        }
+                    }, this);
+                    doRelease(connection);
+                    if (control == 1){
+                        req.session.userData = ret;
+                        console.log("User logged in");
+                        res.redirect('/index');
+                    } else {
+                        res.render("html/login", {cntl: control});
+                    }
+            });
+        });
+    });
 });
 
 app.listen(port, function(error) {
